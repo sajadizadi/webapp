ee55962416ca982efe927b02451b781b4d07f200
sajadizadi <sajad.izadi@bmo.com>
2023-02-17 11:43:24 -0500

    dummy change

--- script.py
+++ script.py
@@ -41,17 +41,3 @@ def getClusters():
         json.dump(config["clusters"], sys.stdout)
 
 
-def fixKubernetesYamls(image, repoName, branchName):
-    with open("infra/app/deployment.yaml") as f_deployment:
-        deployment = yaml.safe_load(f_deployment)
-        print(deployment["metadata"])
-        deployment["metadata"] = {
-            "name": repoName,
-            "namespace": branchName,
-            "labels": {"app": repoName}
-        }
-        deployment["spec"]["selector"] = {"matchLabels": {"app": repoName}}
-
-        deployment["spec"]["template"]["spec"]["containers"][0]["name"] = repoName
-        deployment["spec"]["template"]["spec"]["containers"][0]["image"] = image
-        print(yaml.dump(deployment, default_flow_style=False, sort_keys=False))
