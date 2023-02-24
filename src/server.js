1bad4edf4287ed51513be4e460ea18a0e8f37b67
sajadizadi <sajad.izadi@bmo.com>
2023-02-24 13:39:23 -0500

    change v4

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
     console.log("getting a client")
-    res.send('Hello World v3');
+    res.send('Hello World v4');
 });
 
 app.listen(PORT, HOST, () => {
