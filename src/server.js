a8150d5fa39d5d5ae824fac23d55ef551be45a0d
sajadizadi <sajad.izadi@bmo.com>
2023-02-28 14:57:56 -0500

    removed process

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/process', (req, res) => {
+app.get('/', (req, res) => {
     console.log("getting a client")
     res.send('Hello World');
 });
