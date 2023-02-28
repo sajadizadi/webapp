8c51babd624654f0f9977065249d5bf9da534e56
sajadizadi <sajad.izadi@bmo.com>
2023-02-28 14:52:28 -0500

    added process

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/process', (req, res) => {
     console.log("getting a client")
     res.send('Hello World');
 });
