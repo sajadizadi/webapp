71b1e0fb20ae6d5cb6ab64793056fffcaaaafe78
sajadizadi <sajad.izadi@bmo.com>
2023-02-28 14:45:08 -0500

    v1

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
     console.log("getting a client")
-    res.send('Hello World v10');
+    res.send('Hello World');
 });
 
 app.listen(PORT, HOST, () => {
